35cd37958fcdb9d024c0e289189d123d28783319
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-04-16 18:26:38 +0200

    JAVA

--- Pogadjanje - svaki klijent pogadja posebno/src/app/Client.java
+++ Pogadjanje - svaki klijent pogadja posebno/src/app/Client.java
@@ -12,29 +12,29 @@ public class Client
 
 	public Client() throws Exception
 	{
-		Socket socket=new Socket("192.168.56.1",2019);
+		Socket socket = new Socket("192.168.56.1", 2019);
 		System.out.println("Uspesno povezivanje na server");
-		BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		PrintWriter out=new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
-		Scanner keyboard=new Scanner(System.in);
-		
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+		Scanner keyboard = new Scanner(System.in);
+
 		System.out.println(in.readLine());
-		
-		while(true)
+
+		while (true)
 		{
 			System.out.println(in.readLine());
 			String msg = keyboard.nextLine();
 			out.println(msg);
-			msg=in.readLine();
+			msg = in.readLine();
 			System.out.println(msg);
-			if(msg.contains("Cestitamo"))
+			if (msg.contains("Cestitamo"))
 				break;
 		}
-		
+
 		socket.close();
 		keyboard.close();
 	}
-	
+
 	public static void main(String[] args)
 	{
 		try
